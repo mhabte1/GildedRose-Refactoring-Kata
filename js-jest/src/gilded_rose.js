e7bf5b01e1e4ad2992d86e9346dda627f299aa55
mhabte1 <55032677+mhabte1@users.noreply.github.com>
2023-07-06 22:03:43 -0400

    Assignment 2

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,7 +15,13 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            if (this.items[i].name == 'Conjured') {
+              if(this.items[i].quality > 1){
+              this.items[i].quality = this.items[i].quality - 2;
+              }
+            }
+            else{
+            this.items[i].quality = this.items[i].quality - 1;}
           }
         }
       } else {
@@ -32,7 +38,7 @@ class Shop {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
-          }
+          } 
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -4,6 +4,55 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
+
+  it("normal item - quality decreases by one" , function() {
+    const gildedRose = new Shop([new Item("normal", 10, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(29);
+  });
+
+  it("Aged Brie - quality increases by one", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(31);
+  });
+
+  it("Backstage passes to a TAFKAL80ETC concert - quality increases by TWO", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(32);
+  });
+
+  it("Backstage passes to a TAFKAL80ETC concert - quality increases by THREE", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(33);
+  });
+
+  it("Conjured - quality increases by TWO", function() {
+    const gildedRose = new Shop([new Item("Conjured", 5, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(28);
+  });
+
+  it("Normal item - with SellIn negative and qualtiy greater than zero", function() {
+    const gildedRose = new Shop([new Item("Normal", -3, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(28);
+  });
+  
+  it("Backstage passes to a TAFKAL80ETC concert - SellIn Negative", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", -7, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Aged Brie - SellIn Negative", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", -9, 30)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(32);
+  });
+
 });
